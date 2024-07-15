7a57e3202afa9892502b948b69e0cd6221ff27e0
wahab <wahabrasool08@gmail.com>
2024-07-15 16:07:50 +0500

    Animated search icon

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('circle')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('mag-glass')[0].classList.toggle('active');
+    document.getElementsByClassName('search-input')[0].classList.toggle('active');
+  });
+  
+  document.getElementsByClassName('haste')[0].addEventListener('click', function(event) {
+    document.getElementsByClassName('mag-glass')[0].classList.toggle('active');
+    document.getElementsByClassName('search-input')[0].classList.toggle('active');
+  });
